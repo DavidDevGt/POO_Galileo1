c6575649032a1c6dc1db79821c4deaf89aed1d36
Davidvl <josuedavidvl18@gmail.com>
2023-08-14 23:41:00 -0600

    Usando las clases en Clinica

---
+++ Clinica.java
@@ -0,0 +1,21 @@
+public class Clinica {
+    public static void main(String[] args) {
+        // Creando pacientes
+        Animal perro = new Animal("Firulais", "Perro");
+        Animal gato = new Animal("Mishito Ramirez", "Gato");
+        Animal perico = new Animal("Locutor Martinez", "Perico");
+        Animal gato2 = new Animal("Misifus", "Gato");
+
+        // Creando veterinarios
+        Veterinario doc1 = new Veterinario("Dr. Fernando Muñoz", "101");
+        Veterinario doc2 = new Veterinario("Dra. Isabel Guerra", "022");
+
+        // Asignando y tratando pacientes
+        doc1.asignarPaciente(perro);
+        doc2.asignarPaciente(gato);
+        doc1.tratarPaciente();
+        doc2.tratarPaciente();
+        doc1.asignarPaciente(perico);
+        doc1.asignarPaciente(gato2);
+    }
+}
